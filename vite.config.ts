c272351bd8bcae6896b30aac799433779e1d866a
Paul <57140776+Pawl-Patrol@users.noreply.github.com>
2023-08-13 22:35:26 +0200

    configure rollup, move file to public

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/game-of-life-knot/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        webgpu: "webgpu/index.html",
+        old: "old/index.html",
+      },
+    },
+  },
 });
